bfe4659bb3a02a330c65ff9434b5d1d308d4e6d1
Anandkumar Patel <avpatel2005@gmail.com>
2023-05-11 22:43:19 -0400

    iterate over manuItems and invoke call back fn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const menuContainer = document.querySelector(".menu-container")
+document.addEventListener("DOMContentLoaded", getContent);
 
-fetch("http://localhost:3000/menu")
-.then(resp=>resp.json())
-.then(menuItems=> console.log(menuItems))
+function getContent(){
+    fetch("http://localhost:3000/menu")
+    .then(resp=>resp.json())
+    .then(menuItems=> menuItems.map(menuItem => displayMenuItem(menuItem)))
+}
+
+function displayMenuItem(menuItem){
+    console.log(menuItem)
+}
