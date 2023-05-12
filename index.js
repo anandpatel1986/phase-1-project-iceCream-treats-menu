25e48e4194800faf01fe05f97957aa626ce9b464
Anandkumar Patel <avpatel2005@gmail.com>
2023-05-11 23:45:41 -0400

    add greeting, title, initial filter buttons in html

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const menuContainer = document.querySelector(".menu-container")
+const menuContainer = document.querySelector(".menu-container");
+const filterBtns = document.querySelectorAll(".filter-btn");
 document.addEventListener("DOMContentLoaded", getContent);
 
-function getContent(){
-    fetch("http://localhost:3000/menu")
-    .then(resp=>resp.json())
-    .then(menuItems=> menuItems.map(menuItem => displayMenuItem(menuItem)))
+function getContent() {
+  fetch("http://localhost:3000/menu")
+    .then((resp) => resp.json())
+    .then((menuItems) =>
+      menuItems.forEach((menuItem) => displayMenuItem(menuItem))
+    );
 }
 
-function displayMenuItem(menuItem){
-    const menuItemArticle = document.createElement("article");
-    menuItemArticle.classList = "menu-item";
+function displayMenuItem(menuItem) {
+  const menuItemArticle = document.createElement("article");
+  menuItemArticle.classList = "menu-item";
 
-    const itemImage = document.createElement('img');
-    itemImage.src = menuItem.image;
-    itemImage.alt = menuItem.menuItem;
+  const itemImage = document.createElement("img");
+  itemImage.src = menuItem.image;
+  itemImage.alt = menuItem.menuItem;
 
-    const infoDiv = document.createElement('div');
-    infoDiv.classList = "item-info";
-    const itemTitle = document.createElement('h4');
-    itemTitle.textContent = menuItem.menuItem;
-    const itemPrice = document.createElement('h4');
-    itemPrice.textContent = `$${menuItem.price}`
-    const itemDesc = document.createElement('p');
-    itemDesc.textContent = menuItem.desc;
-    infoDiv.append(itemTitle,itemPrice,itemDesc);
+  const infoDiv = document.createElement("div");
+  infoDiv.classList = "item-info";
+  const itemTitle = document.createElement("h4");
+  itemTitle.textContent = menuItem.menuItem;
+  const itemPrice = document.createElement("h4");
+  itemPrice.textContent = `$${menuItem.price}`;
+  const itemDesc = document.createElement("p");
+  itemDesc.textContent = menuItem.desc;
+  infoDiv.append(itemTitle, itemPrice, itemDesc);
 
-    menuItemArticle.append(itemImage,infoDiv)
-    menuContainer.appendChild(menuItemArticle)
-    
+  menuItemArticle.append(itemImage, infoDiv);
+  menuContainer.appendChild(menuItemArticle);
 }
