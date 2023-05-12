2f1446d93326edf7ee5fe334f8294a5e946cc11b
Anandkumar Patel <avpatel2005@gmail.com>
2023-05-11 22:28:06 -0400

    add fetch to GET request from json server

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("JS file is successfully linked to html")
+const menuContainer = document.querySelector(".menu-container")
+
+fetch("http://localhost:3000/menu")
+.then(resp=>resp.json())
+.then(menuItems=> console.log(menuItems))
